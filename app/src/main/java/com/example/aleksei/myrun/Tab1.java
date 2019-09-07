da52bc61be9751dacd323d849d2f9b04d615c6c3
Aleksei Bingham <asb9189@rit.edu>
2019-09-07 14:55:25 -0400

    Properly setup fragments with working buttons and toast messages.

--- app/src/main/java/com/example/aleksei/myrun/Tab1.java
+++ app/src/main/java/com/example/aleksei/myrun/Tab1.java
@@ -24,6 +24,8 @@ import android.widget.Toast;
  */
 public class Tab1 extends Fragment implements AdapterView.OnItemSelectedListener{
 
+    Button button;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -68,7 +70,6 @@ public class Tab1 extends Fragment implements AdapterView.OnItemSelectedListener
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
     }
 
 
@@ -76,25 +77,29 @@ public class Tab1 extends Fragment implements AdapterView.OnItemSelectedListener
      * onCreateView called after onCreate
      * This is where graphical code goes
      */
-    View layoutView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        layoutView = inflater.inflate(R.layout.fragment_tab1, container, false);
+        View view = inflater.inflate(R.layout.fragment_tab1, container, false);
+        button = (Button) view.findViewById(R.id.tab1_button);
 
-        /*
-        Spinner spinner = (Spinner) getActivity().findViewById(R.id.tab1_distance_spinner);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "FUCK OFF", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        Spinner spinner = (Spinner) view.findViewById(R.id.tab1_distance_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.tab1_units, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
-        //spinner.setOnItemSelectedListener(this);
-        */
-
-
-        return layoutView;
+        return view;
 
     }
 
